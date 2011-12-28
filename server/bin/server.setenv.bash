9dfe88541c11c9b76c6c60026d73948077ed8c54
Adrien LELONG <contact@lelongdunet.com>
2011-12-29 00:39:22 +0100

    Add some comments

--- server/bin/server.setenv.bash
+++ server/bin/server.setenv.bash
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# This script defines common settings for the shareterm server. It's
+# intended to be sourced in other server scripts
+
 SSH=/usr/bin/ssh
 CAT=/bin/cat
 MKDIR=/bin/mkdir
